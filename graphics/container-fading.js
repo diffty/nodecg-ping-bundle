0e8b3c5b74dfeb171469863eaebc41108ce1d9db
diffty <diffty@gmail.com>
2018-10-01 11:32:50 +0200

    container-fading: added customizable onFadeEnd callback, WIP revamped animation calculation system (but still to be partially rewritten)

--- graphics/container-fading.js
+++ graphics/container-fading.js
@@ -13,45 +13,67 @@ class FadingContainer extends PIXI.Container {
 
         this.animTime = 0;
         this.animDuration = 2;
+        this.animStartValue = -1;
+        this.animEndValue = -1;
+        this.animCurrValue = 0;
 
         this.fadeMode = EFadeMode.NOFADE;
+
+        this.onEndFadeCallback = null;
     }
 
     setContent(contentWidget) {
-        self.contentWidget = contentWidget;
-        this.addChild(self.contentWidget);
+        if (this.contentWidget != null && this.children.indexOf(this.contentWidget) >= 0) {
+            this.removeChild(this.contentWidget);
+        }
+
+        this.contentWidget = contentWidget;
+
+        if (this.contentWidget != null) {
+            this.addChild(this.contentWidget);
+        }
     }
 
     fadeIn() {
         app.ticker.add(this.update, this);
         this.animTime = 0;
+        this.animStartValue = 0;
+        this.animEndValue = 1.;
+        this.animCurrValue = this.animStartValue;
         this.fadeMode = EFadeMode.FADEIN;
     }
 
     fadeOut() {
         app.ticker.add(this.update, this);
         this.animTime = 0;
+        this.animStartValue = 1.;
+        this.animEndValue = 0.;
+        this.animCurrValue = this.animStartValue;
         this.fadeMode = EFadeMode.FADEOUT;
     }
 
     onEndFade() {
         app.ticker.remove(this.update, this);
         this.animTime = this.animDuration;
+        if (this.onEndFadeCallback != null) {
+            this.onEndFadeCallback();
+        }
     }
 
     update(delta) {
         this.animTime += app.ticker.elapsedMS / 1000.;
-        let animPercent = this.animTime / this.animDuration;
+        //this.animCurrValue = this.animTime / this.animDuration;
+        this.animCurrValue = this.animStartValue + (this.animEndValue - this.animStartValue) * (this.animTime / this.animDuration);
 
         if (this.animTime > this.animDuration) {
             this.onEndFade();
         }
 
         if (this.fadeMode == EFadeMode.FADEIN) {
-            this.alpha = animPercent;
+            this.alpha = this.animCurrValue;
         }
         else if (this.fadeMode == EFadeMode.FADEOUT) {
-            this.alpha = 1. - animPercent;
+            this.alpha = this.animCurrValue;
         }
     }
 }
