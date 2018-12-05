446cefc634d1c2e4aebf02c4e2f46403bcca3039
diffty <diffty@gmail.com>
2018-12-05 01:46:36 +0100

    ping6: bassthon stinger manual launch, configuration panel in dashboard (with cooldown parameter), arduino serial communication for physical button stinger launch in a nodecg extension

--- extension/index.js
+++ extension/index.js
@@ -1,10 +1,43 @@
 'use strict';
 
+var lastButtonsState = {}
+
 module.exports = function (nodecg) {
-	nodecg.log.info('Hello, from your bundle\'s extension!');
-	nodecg.log.info('I\'m where you put all your server-side code.');
-	nodecg.log.info(`To edit me, open "${__filename}" in your favorite text editor or IDE.`);
-	nodecg.log.info('You can use any libraries, frameworks, and tools you want. There are no limits.');
-	nodecg.log.info('Visit https://nodecg.com for full documentation.');
-	nodecg.log.info('Good luck!');
+	nodecg.log.info('P I N G Overlay System – Extension');
+	nodecg.log.info('Serial port communication extension, for PING6/BassThon');
+
+	const SerialPort = require("serialport");
+	const Readline = require('@serialport/parser-readline')
+
+	var port = new SerialPort("COM12", {
+		baudRate: 9600,
+	});
+
+	var receiveSerialData = (data) => {
+		var splittedData = data.split(":")
+
+		if (!(splittedData[0].indexOf("Button") != -1 && splittedData[1] in ["0", "1"])) {
+			return;
+		}
+
+		var buttonName = splittedData[0];
+		var buttonState = splittedData[1] == "1";
+
+		if (!(buttonName in lastButtonsState)) {
+			lastButtonsState[buttonName] = false;
+		}
+
+		if (buttonState != lastButtonsState[buttonName]) {
+			nodecg.sendMessage("physicalButtonStateChanged", {
+				buttonName: buttonName,
+				buttonState: buttonState,
+			});
+		}
+
+		lastButtonsState[buttonName] = buttonState;
+	}
+
+	const parser = port.pipe(new Readline({ delimiter: '\r\n' }));
+	parser.on('data', receiveSerialData.bind(nodecg));
 };
+
