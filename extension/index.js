2deaf48b4428bcbbc2268eed59a689b0d4eeb24a
diffty <diffty@gmail.com>
2018-12-15 11:26:18 +0100

    changed button detection to allow more buttons, disables serial module if not found

--- extension/index.js
+++ extension/index.js
@@ -6,38 +6,54 @@ module.exports = function (nodecg) {
 	nodecg.log.info('P I N G Overlay System – Extension');
 	nodecg.log.info('Serial port communication extension, for PING6/BassThon');
 
-	const SerialPort = require("serialport");
-	const Readline = require('@serialport/parser-readline')
+	var arduino_enabled = true;
 
-	var port = new SerialPort("COM12", {
-		baudRate: 9600,
-	});
+	const SerialPort = null;
+	const Readline = null;
 
-	var receiveSerialData = (data) => {
-		var splittedData = data.split(":")
+	try {
+		SerialPort = require("serialport");
+		Readline = require('@serialport/parser-readline')
+	}
+	catch (err) {
+		arduino_enabled = false;
 
-		if (!(splittedData[0].indexOf("Button") != -1 && splittedData[1] in ["0", "1"])) {
-			return;
+		if (err.code == 'MODULE_NOT_FOUND') {
+			nodecg.log.info("<!> Modules serialport and/or @serialport/parser-readline has not beed found. Thus, Arduino interface will be disabled.")
 		}
-
-		var buttonName = splittedData[0];
-		var buttonState = splittedData[1] == "1";
-
-		if (!(buttonName in lastButtonsState)) {
-			lastButtonsState[buttonName] = false;
-		}
-
-		if (buttonState != lastButtonsState[buttonName]) {
-			nodecg.sendMessage("physicalButtonStateChanged", {
-				buttonName: buttonName,
-				buttonState: buttonState,
-			});
-		}
-
-		lastButtonsState[buttonName] = buttonState;
 	}
 
-	const parser = port.pipe(new Readline({ delimiter: '\r\n' }));
-	parser.on('data', receiveSerialData.bind(nodecg));
+	if (arduino_enabled == true) {
+		var port = new SerialPort("COM12", {
+			baudRate: 9600,
+		});
+
+		var receiveSerialData = (data) => {
+			var splittedData = data.split(":")
+
+			if (!(splittedData[0].indexOf("Button") != -1 && splittedData[1] in ["0", "1"])) {
+				return;
+			}
+
+			var buttonName = splittedData[0];
+			var buttonState = splittedData[1] == "1";
+
+			if (!(buttonName in lastButtonsState)) {
+				lastButtonsState[buttonName] = false;
+			}
+
+			if (buttonState != lastButtonsState[buttonName]) {
+				nodecg.sendMessage("physicalButtonStateChanged", {
+					buttonName: buttonName,
+					buttonState: buttonState,
+				});
+			}
+
+			lastButtonsState[buttonName] = buttonState;
+		}
+
+		const parser = port.pipe(new Readline({ delimiter: '\r\n' }));
+		parser.on('data', receiveSerialData.bind(nodecg));
+	}
 };
 
